e640e6358f527b76f3fe6e9a442abb9e8bdab68a
dandenney <dd.subscriptions@gmail.com>
2013-02-02 14:39:46 -0500

    2011 Page

--- compass/config.rb
+++ compass/config.rb
@@ -11,6 +11,6 @@ sass_dir = "public/styles/sass" # update to the path of your sass files.
 images_dir = "public/images" # update to the path of your image files.
 javascripts_dir = "public/scripts" # update to the path of your script files.
 
-line_comments = false # if debugging (or using Mixture chrome extension - set this to true)
+line_comments = true # if debugging (or using Mixture chrome extension - set this to true)
 cache = true
 color_output = false # required for Mixture
